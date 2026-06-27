50221e3c2168157ced8a823561a04efb000cdd6c
Syna00 <email-github-synast123@gmail.com>
2026-06-27 11:05:33 +0700

    first commit

---
+++ pertama.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    cout << "hello world" << endl;
+    cout << "yeah";
+    cout << endl << endl << endl;
+    cout << "sinatrio";
+    return 0;
+    
+}
