00ef00dd43b7a65a5cdd78afe0453c424693e833
Syna00 <synast123@gmail.com>
2026-06-27 11:49:40 +0700

    membuat perubahan baru

--- pertama.cpp
+++ pertama.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 int main(){
     cout << "hello world" << endl;
-    cout << "yeah" << endl;
     cout << "sinatrio" << endl << endl << endl;
     cout << "ganteng banget menn";
     return 0;
